04bd50c3acd19246b2d5d2008a41978676746001
Andrew Nascimento <ton.gra.andrew@gmail.com>
2025-12-18 22:08:28 -0300

    Commit para unir o repositorio local com o do github.

--- conversor-termico.c
+++ conversor-termico.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <cs50.h>
 
+float temp_celsius;
+float temp_farenheit;
+
 int main(void)
 {
     printf("Olá, este software é destinado à conversão de unidades de calor como Celsius e Farenheit\n");
     printf("Escolha a conversão desejada: \n");
     printf("1. Conversão de Celsius para Farenheit \n");
     printf("2. Conversão de Farenheit para Celsius\n");
-    int tipo_conversao = get_int("");
-    float temp_celsius;
-    float temp_farenheit;
+    int tipo_conversao; 
+    
+    do
+    {
+        tipo_conversao = get_int("Insira uma das opções: ");
+        printf("\n");
+    } 
+    while (tipo_conversao < 1 || tipo_conversao >= 3);
     if (tipo_conversao == 1)
     {
-        printf("Tudo certo, vamos converter de Celsius para Farenheit!\n");
-        temp_celsius = get_float("Qual a temperatura em Celsius? ");
-        temp_farenheit = (1.8*temp_celsius) + 32;
-        printf("A temperatura em Farenheit é %f\n", temp_farenheit);
-
-    } else 
+        c_to_f();
+    } else
     {
-        printf("Tudo certo, vamos converter de Farenheit para Celsius!\n");
-        temp_farenheit = get_float("Qual a temperatura em Farenheit? ");
-        temp_celsius = (temp_farenheit - 32)/1.8;
-        printf("A temperatura em Celsius é %f\n", temp_celsius);
+        f_to_c();
     }
     return 0;
+}
+
+float c_to_f (void){
+    printf("Tudo certo, vamos converter de Celsius para Farenheit!\n");
+    temp_celsius = get_float("Qual a temperatura em Celsius? ");
+    temp_farenheit = (1.8*temp_celsius) + 32;
+    printf("A temperatura em Farenheit é %f\n", temp_farenheit);
+
+} 
+float f_to_c (void){
+    printf("Tudo certo, vamos converter de Farenheit para Celsius!\n");
+    temp_farenheit = get_float("Qual a temperatura em Farenheit? ");
+    temp_celsius = (temp_farenheit - 32)/1.8;
+    printf("A temperatura em Celsius é %f\n", temp_celsius);
+
 }
